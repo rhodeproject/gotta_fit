2a5b59cb89b1a1eef43dca626b994de04d30c877
Matt Hatch <mhatch73@gmail.com>
2012-05-17 07:10:51 -0400

    display static pages

--- Gemfile
+++ Gemfile
@@ -2,10 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
 gem 'pg'
+gem 'bootstrap-sass', '2.0.0'
 
-
-# Gems used only for assets and not required
-# in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe "HomePages" do
   describe "GET /" do
+    let(:base_title) {'gotta fit'}
     it "displays Welcome Page" do
       get root_path
-      response.body.should include("Welcome")
-      response.title.should include("gotta fit")
+      response.body.should include('gotta_fit')
+      response.body.should include('home')
     end
   end
 end
