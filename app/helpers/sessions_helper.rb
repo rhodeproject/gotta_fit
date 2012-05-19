11c22733828ba4f56ddc028938463e1192403166
Matt Hatch <mhatch73@gmail.com>
2012-05-19 14:43:50 -0400

    fix active user issue

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,10 @@
 module SessionsHelper
   def sign_in(user)
-    cookies.permanent[:remember_token] = user.remember_token
-    current_user = user
+    if user.active?
+      cookies.permanent[:remember_token] = user.remember_token
+      current_user = user
+    end
+
   end
 
   def sign_out
