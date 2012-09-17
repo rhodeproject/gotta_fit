e8a6c93688ffe6fe035adb8d84e40d1ce8ed0782
Matt Hatch <mhatch73@gmail.com>
2012-09-17 18:01:33 -0400

    included javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.datepicker
-//= require jquery-ui-timepicker-addon
 //= require fullcalendar
 //= require dataTables/jquery.dataTables
 //= require_tree .
